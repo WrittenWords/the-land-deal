65798f7a0a417ec4df7f4652e3af756985d1820d
WrittenWords <278225020+WrittenWords@users.noreply.github.com>
2026-07-02 22:52:15 -0400

    share URL points at the live site

--- js/links.js
+++ js/links.js
@@ -8,6 +8,6 @@ const LINKS = {
   newsletter: 'https://signalohio.org/newsletters',       // TODO-LINK: confirm signup URL
   publicRecordsLaw: 'https://www.ohioattorneygeneral.gov/Legal/Sunshine-Laws', // Ohio Sunshine Laws (real, stable)
   sosBusinessSearch: 'https://businesssearch.ohiosos.gov/',                    // Ohio SOS business search (real, stable)
-  gameUrl: 'https://example.github.io/landdeal/'          // TODO-LINK: final Pages URL
+  gameUrl: 'https://writtenwords.github.io/the-land-deal/'
 }
 if (typeof module !== 'undefined') { module.exports = { LINKS } } else { window.LINKS = LINKS }
